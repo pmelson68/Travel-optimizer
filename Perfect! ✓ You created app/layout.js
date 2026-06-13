931ed1f43213900fb47d89444c96ae86039917f7
pmelson68 <pmelson68@gmail.com>
2026-06-12 22:30:47 -0500

    Create layout.js

---
+++ Perfect! ✓ You created app/layout.js
@@ -0,0 +1,14 @@
+```javascript
+export const metadata = {
+title: 'Flight Miles Optimizer',
+description: 'Find the best flights with airline miles and credit card points'
+}
+
+export default function RootLayout({ children }) {
+return (
+<html lang="en">
+<body>{children}</body>
+</html>
+)
+}
+```
